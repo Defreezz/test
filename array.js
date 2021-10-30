5564834b22da6ab11521c1daa54cf8b820c93340
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-31 01:47:53 +0300

    update

--- array.js
+++ array.js
@@ -1,41 +1,13 @@
-// Setup
-const contacts = [
-    {
-        firstName: "Akira",
-        lastName: "Laine",
-        number: "0543236543",
-        likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-        firstName: "Harry",
-        lastName: "Potter",
-        number: "0994372684",
-        likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-        firstName: "Sherlock",
-        lastName: "Holmes",
-        number: "0487345643",
-        likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-        firstName: "Kristian",
-        lastName: "Vos",
-        number: "unknown",
-        likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-];
 
-function lookUpProfile(name, prop) {
-    for (let i = 0; i > contacts.length; i++){
-        const con = contacts[i];
-        if (con.firstName === name && con.hasOwnProperty(prop)) {
-            return con[prop];
-        } else if (con.firstName === name && !con.hasOwnProperty(prop)) {
-            return "No such property";
+    function countdown(n) {
+        if (n < 1) {
+            return [];
+        } else {
+            const countArray = countdown(n - 1);
+            countArray.push(n);
+            return countArray;
         }
     }
-    return "No such contact";
-}
 
-lookUpProfile("Kristian", "lastName");
+
+console.log(countdown(5));
