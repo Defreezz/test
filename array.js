add3f6233a92f5e498d39975b86fb1e4a569f5fa
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-30 23:46:39 +0300

    update

--- array.js
+++ array.js
@@ -1,38 +1,16 @@
-const myArray = [];
-let i = 5
-while (i > -1) {
-    myArray.push(i);
-    i--;
-}
-console.log(myArray);
-
-
-const myArrayy = [];
-
-for (let i = 1; i < 6; i++){
-    myArrayy.push(i);
-}
-console.log(myArrayy);
-
-//нечетные
-const ourArray = [];
-
-for (let i = 0; i < 10; i++) {
-   if (i%2 !== 0) ourArray.push(i);
-}
-console.log(ourArray);
-
-const myArraya = [];
-for (let i = 10; i > 0; i--) {
-    if (i%2 !== 0){
-        myArraya.push(i);
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            console.log(arr[i][j]);
+            product *= arr[i][j] ;
+        }
     }
-}
-console.log(myArraya);
+    // Only change code above this line
+    console.log(product);
 
-const myArr = [2, 3, 4, 5, 6];
-let total = 0;
-for (let i = 0; i < myArr.length; i++ ){
-    total += myArr[i]; // total = total + myArr[i]
+    return product;
 }
-console.log(total);
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
