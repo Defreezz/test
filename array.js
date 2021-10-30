94db74ca2251ea5a28619efb33514838ff65b976
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-30 23:30:09 +0300

    update

--- array.js
+++ array.js
@@ -21,3 +21,18 @@ for (let i = 0; i < 10; i++) {
    if (i%2 !== 0) ourArray.push(i);
 }
 console.log(ourArray);
+
+const myArraya = [];
+for (let i = 10; i > 0; i--) {
+    if (i%2 !== 0){
+        myArraya.push(i);
+    }
+}
+console.log(myArraya);
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++ ){
+    total += myArr[i]; // total = total + myArr[i]
+}
+console.log(total);
