e0b55d60075209bd15f8c1a103416be53fc9d736
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-30 22:25:46 +0300

    update

--- Record Collection.js
+++ Record Collection.js
@@ -26,10 +26,10 @@ function updateRecords(records, id, prop, value) {
     } else if (prop === "tracks" && !records[id].hasOwnProperty("tracks")) {
         records[id][prop] = [value] ;
     } else if (prop === "tracks" && value !=="") {
-        records[id][prop].push(value);
+    records[id][prop].push(value);
     } else if (value === "") {
-        delete records[id][prop];
-    }
+    delete records[id][prop];
+}
     return records;
 }
 
