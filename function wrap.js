79ae69be63dc5a44ae909a5e9653c1e8b3fd3835
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-31 14:39:22 +0300

    update

--- function wrap.js
+++ function wrap.js
@@ -1,4 +1,4 @@
-
+/*
 function wrap(value) {
     const wp ={
 
@@ -8,3 +8,19 @@ function wrap(value) {
 }
 wrap("sas");
 console.log(wp);
+
+function greet(a,b){
+    let c =  a + " " + b;
+    return c;
+};
+
+
+console.log(greet("hello", "world!"));
+function greett(a){
+    //let c = a + " " + b;
+    return a;
+};
+console.log(greett("hello world!"));
+*/
+items = []
+items.push {a: "b", c: "d"}
