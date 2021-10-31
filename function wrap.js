0052850d238e234225d9e0f43dcd43677c0d83a0
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-31 18:11:20 +0300

    update

--- function wrap.js
+++ function wrap.js
@@ -22,5 +22,27 @@ function greett(a){
 };
 console.log(greett("hello world!"));
 */
-items = []
-items.push {a: "b", c: "d"}
+/*
+function areYouPlayingBanjo(name) {
+    if (name[0] === "R" || name[0] === "r"){
+        return name + " plays banjo"
+    }
+    return name;
+}
+console.log(areYouPlayingBanjo("Ringo"));
+*/
+//const areYouPlayingBanjo = name => name[0] === "R" || name[0] === "r" ? name + " plays banjo" : name + " does not play banjo";
+//console.log(areYouPlayingBanjo("Ringo"));
+function grader(score) {
+    if (score > 1 || score < 0.6){
+        return "F";
+    }else if (score === 0.9) {
+        return "A";
+    }else if (score === 0.8) {
+        return "B";
+    }else if (score === 0.7) {
+        return "C";
+    }else {
+        return "D";}
+}
+console.log(grader(0.6));
