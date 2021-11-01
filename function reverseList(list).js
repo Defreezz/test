07f5df1fc60500843c64307ce1d3772cae604a52
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-11-01 23:15:27 +0300

    update 1.11

--- function reverseList(list).js
+++ function reverseList(list).js
@@ -1,11 +1,20 @@
 function reverseList(list) {
-   let b = list.reverse();
+   let b = list.reverse();// return list.reverse()
     //console.log(b);
     return b;
 }
-reverseList([1,2,3,4]);
+reverseListTwo([1,2,3,4]);
 
 
- var a = [3,5,7,8]
-var b = a.reverse();
+let a = [3,5,7,8]
+let b = a.reverse();
 console.log(b);
+
+function reverseListTwo(list) {
+    var array = [];
+    for (var i = list.length-1; i >= 0; i --){
+        array.push(list[i]);
+    }
+    return array;
+}
+console.log(reverseListTwo([1,2,3,4,5,6,7,8,1]));
