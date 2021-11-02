28790ddfdbb527101fa7b59774453272516191f1
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-11-02 23:39:27 +0300

    up

--- function makeNegative(num).js
+++ function makeNegative(num).js
@@ -3,3 +3,13 @@ function makeNegative(num) {
     return num<0 ? num: Number(-num);
 }
 console.log(makeNegative(9));
+
+var repeatIt = function(str, n) {
+   // return str.repeat(n);
+    if (typeof str === "string"){
+        console.log("строка");
+}else {
+        console.log("не строка")
+    }
+}
+console.log(repeatIt ("***", 2))
