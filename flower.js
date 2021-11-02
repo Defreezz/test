3027d46e15448b22aaeae8953b7044e7ccb810e9
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-11-02 23:08:25 +0300

    up

--- flower.js
+++ flower.js
@@ -2,6 +2,6 @@ function lovefunc(flower1, flower2){
     // moment of truth
 
 
-    return flower1 + 2 % 2 !== 0 && flower2 % 2 === 0;}
+    return flower1 + flower2 % 2 !== 0;}
 console.log(lovefunc(1, 0));
 console.log(lovefunc(1, 4));
