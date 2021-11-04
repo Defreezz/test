c64c4ed139572330518906c3fa1e4cca77757c78
defreezz <yatsevich-artsiom@yandex.by>
2021-11-04 19:56:30 +0300

    up

--- grade.js
+++ grade.js
@@ -1,13 +1,7 @@
-function getGrade (s1, s2, s3) {
-    let sum  = (s1 + s2 + s3) / 3
-    if(sum >= 90 && sum <= 100){
-        return 'A'
-    } else if (sum >= 80 && sum <= 90){
-        return 'B'
-    } else if (sum >=70 && sum <= 80){
-        return 'C'
-    } else if (sum >= 60 && sum <= 70) {
-        return 'D'
-    } else return 'F'
+let arr = [2, 5, 6, 10];
 
-}
+let result = arr.reduce(function(sum ,item, index, array){
+    return sum += item / arr.length;
+});
+
+console.log(result);
