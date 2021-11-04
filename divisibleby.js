b29a255a5e0555f3512d62a94fa2ae14c3acb408
defreezz <yatsevich-artsiom@yandex.by>
2021-11-04 21:08:15 +0300

    up

--- divisibleby.js
+++ divisibleby.js
@@ -2,7 +2,7 @@ function divisibleBy(numbers, divisor){
     let result = [];
     for (let i = 0; i<=numbers.length; i++){
         if(numbers[i]%divisor === 0 ){
-            result.push(i)
+            result.push(numbers[i])
         }
 
     }
@@ -10,4 +10,4 @@ function divisibleBy(numbers, divisor){
     return result;
 }
 
-divisibleBy([2,4,3,,5,], 2)
+divisibleBy([2,4,3,,5,6], 3)
